a78015d8a663aa5b696b52261c64d03fc7075852
Sidoine De Wispelaere <sidoine@sidoine.net>
2020-12-04 13:40:18 +0100

    Add lua section

--- index.d.ts
+++
@@ -1,6 +0,0 @@
-import { LuaObj } from "@wowts/lua";
-export declare class LibBabbleCreatureType {
-    GetLookupTable(): LuaObj<string>;
-}
-declare const lib: LibBabbleCreatureType;
-export default lib;
